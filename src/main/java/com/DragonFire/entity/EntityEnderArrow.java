e939cfab1f167c32b10140a5f7a118ad5a3c92e5
SirBlobman <blobmansrealm@gmail.com>
2017-08-20 17:40:51 -0400

    21
    Improved ender arrow teleportation

--- src/main/java/com/DragonFire/entity/EntityEnderArrow.java
+++ src/main/java/com/DragonFire/entity/EntityEnderArrow.java
@@ -4,8 +4,10 @@ import com.DragonFire.item.DFItems;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
@@ -23,12 +25,17 @@ public class EntityEnderArrow extends EntityArrow {
     public void onHit(RayTraceResult rtr) {
         super.onHit(rtr);
         Entity shooter = this.shootingEntity;
-        if(shooter != null && shooter instanceof EntityLivingBase) {
+        if(shooter != null) {
             double x = posX;
             double y = posY + 1.0D;
             double z = posZ;
-            EntityLivingBase elb = (EntityLivingBase) shooter;
-            elb.attemptTeleport(x, y, z);
+            float yaw = shooter.rotationYaw;
+            float pit = shooter.rotationPitch;
+            if(shooter instanceof EntityPlayerMP) {
+                EntityPlayerMP player = (EntityPlayerMP) shooter;
+                NetHandlerPlayServer server = player.connection;
+                server.setPlayerLocation(x, y, z, yaw, pit);
+            } else shooter.setLocationAndAngles(x, y, z, yaw, pit);
             setDead();
         }
     }
