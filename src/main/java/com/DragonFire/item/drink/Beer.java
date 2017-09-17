91ee08e02bebe4d7c1f73194cf06d3c45f758ec0
SirBlobman <blobmansrealm@gmail.com>
2017-09-17 00:51:35 -0400

    44
    Added Tough as Nails support
    Beer will now heal 1 thirst (1/2 drop)

--- src/main/java/com/DragonFire/item/drink/Beer.java
+++ src/main/java/com/DragonFire/item/drink/Beer.java
@@ -1,5 +1,7 @@
 package com.DragonFire.item.drink;
 
+import com.DragonFire.compat.ToughAsNails.TANUtil;
+
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
@@ -34,6 +36,9 @@ public class Beer extends QuickDrink {
                 nbt.setInteger("BeersDrunk", 0);
                 ep.attackEntityFrom(BEER, Float.MAX_VALUE);
             }
+            
+            try {TANUtil.addThirst(ep, 1);}
+            catch(Throwable ex) {}
         }
         return super.onItemUseFinish(is, w, elb);
     }
