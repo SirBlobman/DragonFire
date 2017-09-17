d7036af68149bdeac9bd9acf1d81054023909908
SirBlobman <blobmansrealm@gmail.com>
2017-09-17 16:24:26 -0400

    49
    Fixed obsidian glass rendering too many sides

--- src/main/java/com/DragonFire/block/BlockObsidianGlass.java
+++ src/main/java/com/DragonFire/block/BlockObsidianGlass.java
@@ -1,10 +1,12 @@
 package com.DragonFire.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
@@ -19,4 +21,14 @@ public class BlockObsidianGlass extends QuickBlock {
     public boolean isFullCube(IBlockState ibs) {return false;}
     public boolean isOpaqueCube(IBlockState ibs) {return false;}
     public boolean canEntityDestroy(IBlockState ibs, IBlockAccess iba, BlockPos bp, Entity en) {return false;}
+    
+    @Override
+    @SuppressWarnings("deprecation")
+    public boolean shouldSideBeRendered(IBlockState ibs, IBlockAccess iba, BlockPos bp, EnumFacing ef) {
+        IBlockState bs = iba.getBlockState(bp.offset(ef));
+        Block b = bs.getBlock();
+        if(ibs != bs) return true;
+        if(b == this) return false;
+        return ((b == this) ? false : super.shouldSideBeRendered(ibs, iba, bp, ef));
+    }
 }
