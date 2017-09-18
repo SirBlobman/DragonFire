6e9ce8d20b656304c1d4c6c69b3e90ce166aa09e
SirBlobman <blobmansrealm@gmail.com>
2017-09-17 20:04:01 -0400

    49.1
    Changed confusion potion color to light orange

--- src/main/java/com/DragonFire/potion/effect/PotionConfusion.java
+++ src/main/java/com/DragonFire/potion/effect/PotionConfusion.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import net.minecraft.entity.EntityLivingBase;
 
 public class PotionConfusion extends QuickPotion {
-    public PotionConfusion() {super("confusion", true, Util.getRGB(0, 255, 0), 0, 0);}
+    public PotionConfusion() {super("confusion", true, Util.getRGB(244, 124, 12), 0, 0);}
     public boolean isReady(int duration, int amplifier) {return (duration > 0);}
     
     @Override
