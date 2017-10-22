0fd933eadb4bf6f1fbe9a61d6bf4cff0e69b6407
SirBlobman <blobmansrealm@gmail.com>
2017-10-21 23:06:09 -0400

    .

--- src/main/java/com/DragonFire/network/gui/GuiNetherBrickFurnace.java
+++ src/main/java/com/DragonFire/network/gui/GuiNetherBrickFurnace.java
@@ -53,6 +53,7 @@ public class GuiNetherBrickFurnace extends GuiContainer {
     LABEL_Y = 5;
     
     @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         drawDefaultBackground();
         super.drawScreen(mouseX, mouseY, partialTicks);
         renderHoveredToolTip(mouseX, mouseY);
